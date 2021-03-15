eab9210fc4ca3d7dad0600cba098a7fd24da9ff2
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-15 10:24:48 -0300

    feat: added GlobalStyle and styled-components

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello dtmoneyy</h1>
+      <GlobalStyle/>
     </div>
   );
 }
