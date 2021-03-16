8412ccece8016157c0bcf93801a34d9d029f649e
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-16 11:53:53 -0300

    feat: added react-modal styles

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,15 +1,29 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
-interface newTransactionModalProps{
+interface newTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}:newTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: newTransactionModalProps) {
 
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-            <h1>Cadastrar transação</h1>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+            >
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input type="text" placeholder="Título" />
+                <input type="number" placeholder="Valor" />
+                <input type="text" placeholder="Categoria" />
+
+                <button type="submit">Cadastrar</button>
+            </Container>
         </Modal>
     )
 }
