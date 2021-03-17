49c7b33b5edf8694c936b303dd339c6803f4110b
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-17 10:42:23 -0300

    feat: added summary total calculator

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -9,7 +9,25 @@ import { TransactionsContext } from "../../TransactionsContext";
 export function Summary() {
     
     const { transactions } = useContext(TransactionsContext);
-    
+
+    const {deposits, total, withdraws } = transactions.reduce((acc, transaction) => {
+        if(transaction.type === 'deposit'){
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else if(transaction.type === 'withdraw'){
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    },{
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    })
+
+   
+
     return (
         <Container>
             <div>
@@ -17,21 +35,33 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={incomeImage} alt="Entradas" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>{new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(total)}
+                </strong>
             </div>
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImage} alt="Entradas" />
                 </header>
-                <strong>-R$500,00</strong>
+                <strong>-{new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(withdraws)}
+                </strong>
             </div>
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImage} alt="Entradas" />
                 </header>
-                <strong>R$500,00</strong>
+                <strong>{new Intl.NumberFormat('pt-BR', {
+                                style: 'currency',
+                                currency: 'BRL'
+                            }).format(total)}
+                </strong>
             </div>
         </Container>
     )
